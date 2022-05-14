4a6930235bd2e62e17f20f684787f3b8129c0aed
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-05-13 21:59:04 -0500

    Update __dict_extract method

--- utils/dict.py
+++ utils/dict.py
@@ -1,21 +1,30 @@
-def partial(value: dict, schema) -> dict:
-    return __dict_fetch(value, schema)
+def __dict_fetch(dictionary: dict, key, to_dict=True, raises=False) -> dict:
+    if dictionary is None:
+        return
 
+    __dict = dictionary
 
-def __dict_fetch(_dict: dict, key) -> dict:
+    if not isinstance(__dict, dict):
+        if to_dict:
+            if hasattr(__dict, 'to_dict'):
+                __dict = dictionary.to_dict()
+            elif raises:
+                raise TypeError(
+                    f'Object with key {key} is of type <{type(__dict)}>, and cannot be transformed to <dict>')
+            else:
+                return
+        else:
+            return
+
     if isinstance(key, list):
-        return {_key: _val for k in key for _key, _val in __dict_fetch(_dict, k).items()}
+        return {key_key: key_val for k in key for key_key, key_val in __dict_fetch(__dict, k).items()}
 
     if isinstance(key, dict):
-        return {k: __dict_fetch(_dict.get(k), v) for k, v in key.items()}
-        # return {_key: _val for k, v in key.items() for _key, _val in __dict_fetch(_dict.get(k), v).items()}
+        return {k: __dict_fetch(__dict.get(k), v) if __dict else None for k, v in key.items()}
 
     else:
-        return {key: _dict.get(key)}
-
-# def __dict_fetch_dict(_dict: dict, key, value)-> dict:
-#   val
+        return {key: __dict.get(key)}
 
 
-dic = {'a': 3, 'b': {'c': 1, 'd': 2}}
-print(partial(dic, ['a', {'b': 'd'}]))
+def partial_dict(_dict: dict, schema, to_dict=True, raises=False) -> dict:
+    return __dict_fetch(_dict, schema, to_dict, raises)
