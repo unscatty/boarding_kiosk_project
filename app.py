841c377de8152b7a8c65fb501f14eee376e16612
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-05-13 23:56:58 -0500

    Add ID recognition endpoint

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-import json
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from services import form_recognizer
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return jsonify({'name': 'alice', 'email': 'alice@gmail.com'})
+
+
+@app.route('/identity', methods=['GET'])
+def recognize_id():
+    content_url = request.args.get('url')
+    
+    return jsonify(form_recognizer.get_identity(content_url))
 
 
 app.run(debug=True)
